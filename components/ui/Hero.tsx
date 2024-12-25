fbc25f4db6899f2ff35410f4afea8ab295ce8680
Raquelbateman <aliciaraquel1017@gmail.com>
2024-12-24 22:04:01 -0800

    Initial commit

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Spotlight } from "./Spotlight";
 
-const Hero = () => {
+export const Hero = () => {
   return (
     <>
       <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Hero from "@/components/ui/Hero";
+
+import Hero from "components/ui/Hero";
 import Image from "next/image";
 
 export default function Home() {
