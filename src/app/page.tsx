4d7fd49af78b8c0ad9161462a29aba1ec1d9896a
Raquelbateman <aliciaraquel1017@gmail.com>
2024-12-25 23:29:25 -0800

    trying to commit from termical

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 
+import { FloatingNav } from "components/ui/FloatingNav";
+import Grid from "components/ui/Grid";
 import Hero from "components/ui/Hero";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative h-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
       <h1 className="text-white-200"></h1>
+      <FloatingNav navItems={[{name:"Home",link:"/",icon:<FaHome/>}]}/>
       <Hero/>
+      <Grid/>
     </div>
    </main>
 
