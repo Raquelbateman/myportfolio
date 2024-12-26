1aefa97b98c338eac84055546f2ce4e5de1a914a
Raquelbateman <aliciaraquel1017@gmail.com>
2024-12-25 21:34:07 -0800

    Rebuilding repository

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="relative h-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
-      <h1 className="text-white-200">Hello, Portfolio</h1>
+      <h1 className="text-white-200"></h1>
       <Hero/>
     </div>
    </main>
